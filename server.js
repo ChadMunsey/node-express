26583659edeb72a6a6057f5341008b481f13c445
Chad Munsey <munsey.chad@gmail.com>
2021-07-22 20:09:56 -0500

    Intro To Express Part 2

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express(); //returns an express server app now available under the variable name app
+app.use(morgan('dev')); //configure morgan middleware to log using the dev version
+
+//setup express to serve files from the public folder using middleware function express.static
+app.use(express.static(__dirname + '/public')); 
 
 //set up server to return the same response for any request
 //app.use accepts a callback function known as a middleware function
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode= 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
